04429438570544e2eca87514491e5423ac6698b4
acgotaku <acgotaku311@gmail.com>
2023-03-03 22:21:31 +0900

    feat: input params

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button';
 export { default as message } from './message';
 export { default as QrReader } from './QrReader';
+export { default as Textarea } from './Textarea';
--- src/views/QRCode/QRCode.tsx
+++ src/views/QRCode/QRCode.tsx
@@ -1,5 +1,49 @@
+import { useState } from 'react';
+import { Textarea } from '@/components';
+import styles from './code.module.css';
+
 const QRCodeView = () => {
-  return <div>{'QR code '}</div>;
+  const [text, setText] = useState('Hello, world!');
+  const [level, setLevel] = useState('Q');
+  const [version, setVersion] = useState('12');
+  return (
+    <div className={styles.code}>
+      <h1 className={styles.title}>Generator QR code</h1>
+      <div className={styles.container}>
+        <div className={styles.item}>
+          <label className={styles.label}>Text</label>
+          <Textarea
+            value={text}
+            onChange={event => setText(event.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Level</label>
+          <select
+            className={styles.select}
+            value={level}
+            onChange={e => setLevel(e.target.value)}
+          >
+            <option value="L">LOW</option>
+            <option value="M">MEDIUM</option>
+            <option value="Q">QUARTILE</option>
+            <option value="H">HIGH</option>
+          </select>
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Version</label>
+          <input
+            value={version}
+            className={styles.input}
+            onChange={e => setVersion(e.target.value)}
+            type="number"
+            min={1}
+            max={40}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default QRCodeView;
